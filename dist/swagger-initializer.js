e3f735b2a5d887216b4d9eca1d18f84358fe735d
Jürgen Dooms <jurgen.dooms@vlaanderen.be>
2024-05-31 10:20:09 +0200

    Added definitions

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    urls: [
+	{
+		name : "Melding Centrale Stooktoestellen voor Woningpas API",
+		url: "/api-specs/datadienstenv1.yaml"
+	},
+	{
+		name : "Energie attesten API",
+		url: "/api-specs/energie-attesten-api-docs.json"
+	}
+	],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
