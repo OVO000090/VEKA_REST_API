eeb43b81445ea279ee5fcbffb92ac126494ebdb6
Raf Lauriks <raf.lauriks@cegeka.com>
2024-06-13 21:43:30 +0200

    Changes

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
-# VEKA REST APIs
+# VEKA REST API's
 
-Overzicht van de externe partner APIs aangeboden door VEKA
-
-## API Documentatie TEST
-
-* [Melding Centrale Stooktoestellen voor Woningpas API](/docs/mcs-wp-api-test-v1.md)
-
+Overzicht van de publieke VEKA API's.
 
 ## API Documentatie UAT
 
--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,10 +4,6 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-	{
-		name : "V1 - Melding Centrale Stooktoestellen voor Woningpas API - TEST",
-		url: "datadiensten_test_v1.yaml"
-	},
     {
 		name : "V1 - Melding Centrale Stooktoestellen voor Woningpas API - UAT",
 		url: "datadiensten_uat_v1.yaml"
